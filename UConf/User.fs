4dc9418d1f21d0febf8223cc02573101ec0832fb
Cracklings home <Cracklings3D@gmail.com>
2022-08-12 02:06:51 +0800

    wip

---
+++ UConf/User.fs
@@ -0,0 +1,20 @@
+﻿module User
+
+open System
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+
+
+type EngineInstall =
+  {
+    Version: Version
+    RootPath: Uri
+    Plugins: PluginInstall seq
+  }
+
+type UserConf =
+  {
+    EngineInstalls: EngineInstall seq
+    SvnExecutable: Uri
+  }
